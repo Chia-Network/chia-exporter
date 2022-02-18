540b7fb973fb5c0a6ecc3854cb5fe93468dceee1
Chris Marslender <chrismarslender@gmail.com>
2022-02-18 17:52:54 -0600

    Add metric descriptions

--- internal/metrics/crawler.go
+++ internal/metrics/crawler.go
@@ -30,11 +30,11 @@ type CrawlerServiceMetrics struct {
 // InitMetrics sets all the metrics properties
 func (s *CrawlerServiceMetrics) InitMetrics() {
 	// Crawler Metrics
-	s.totalNodes5Days = s.metrics.newGauge(chiaServiceCrawler, "total_nodes_5_days", "")
+	s.totalNodes5Days = s.metrics.newGauge(chiaServiceCrawler, "total_nodes_5_days", "Total number of nodes that have been gossiped around the network with a timestamp in the last 5 days. The crawler did not necessarily connect to all of these peers itself.")
 	s.reliableNodes = s.metrics.newGauge(chiaServiceCrawler, "reliable_nodes", "reliable nodes are nodes that have port 8444 open and have available space for more peer connections")
-	s.ipv4Nodes5Days = s.metrics.newGauge(chiaServiceCrawler, "ipv4_nodes_5_days", "")
-	s.ipv6Nodes5Days = s.metrics.newGauge(chiaServiceCrawler, "ipv6_nodes_5_days", "")
-	s.versionBuckets = s.metrics.newGaugeVec(chiaServiceCrawler, "version_bucket", "", []string{"version"})
+	s.ipv4Nodes5Days = s.metrics.newGauge(chiaServiceCrawler, "ipv4_nodes_5_days", "Total number of IPv4 nodes that have been gossiped around the network with a timestamp in the last 5 days. The crawler did not necessarily connect to all of these peers itself.")
+	s.ipv6Nodes5Days = s.metrics.newGauge(chiaServiceCrawler, "ipv6_nodes_5_days", "Total number of IPv6 nodes that have been gossiped around the network with a timestamp in the last 5 days. The crawler did not necessarily connect to all of these peers itself.")
+	s.versionBuckets = s.metrics.newGaugeVec(chiaServiceCrawler, "version_bucket", "Number of peers for each version. Only peers the crawler was able to connect to are included here.", []string{"version"})
 }
 
 // InitialData is called on startup of the metrics server, to allow seeding metrics with
