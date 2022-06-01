32516430812efa73821c39bcfa9e995a4755e7d1
Chris Marslender <chrismarslender@gmail.com>
2022-06-01 11:21:47 -0500

    Handle submitted_partial

--- internal/metrics/farmer.go
+++ internal/metrics/farmer.go
@@ -1,8 +1,11 @@
 package metrics
 
 import (
+	"encoding/json"
+
 	"github.com/chia-network/go-chia-libs/pkg/types"
 	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
 )
 
 // Metrics that are based on Farmer RPC calls are in this file
@@ -16,7 +19,6 @@ type FarmerServiceMetrics struct {
 	submittedPartials   *prometheus.CounterVec
 	currentDifficulty   *prometheus.GaugeVec
 	pointsAckSinceStart *prometheus.GaugeVec
-	pointsAck24h        *prometheus.GaugeVec
 }
 
 // InitMetrics sets all the metrics properties
@@ -26,19 +28,13 @@ func (s *FarmerServiceMetrics) InitMetrics() {
 	s.submittedPartials = s.metrics.newCounterVec(chiaServiceFarmer, "submitted_partials", "Number of partials submitted since the exporter was started", poolLabels)
 	s.currentDifficulty = s.metrics.newGaugeVec(chiaServiceFarmer, "current_difficulty", "Current difficulty for this launcher id", poolLabels)
 	s.pointsAckSinceStart = s.metrics.newGaugeVec(chiaServiceFarmer, "points_acknowledged_since_start", "Points acknowledged since start. This is calculated by chia, NOT since start of the exporter.", poolLabels)
-	s.pointsAck24h = s.metrics.newGaugeVec(chiaServiceFarmer, "points_acknowledged_24h", "Points acknowledged in the past 24h. This is calculated by chia, doesn't rely on exporter being up > 24h.", poolLabels)
 }
 
 // InitialData is called on startup of the metrics server, to allow seeding metrics with current/initial data
 func (s *FarmerServiceMetrics) InitialData() {}
 
 // Disconnected clears/unregisters metrics when the connection drops
-func (s *FarmerServiceMetrics) Disconnected() {
-	s.submittedPartials.Reset()
-	s.currentDifficulty.Reset()
-	s.pointsAckSinceStart.Reset()
-	s.pointsAck24h.Reset()
-}
+func (s *FarmerServiceMetrics) Disconnected() {}
 
 // ReceiveResponse handles crawler responses that are returned over the websocket
 func (s *FarmerServiceMetrics) ReceiveResponse(resp *types.WebsocketResponse) {
@@ -46,11 +42,21 @@ func (s *FarmerServiceMetrics) ReceiveResponse(resp *types.WebsocketResponse) {
 	case "submitted_partial":
 		s.SubmittedPartial(resp)
 	case "proof":
+		log.Printf("%+v", resp)
 		// @TODO
 	}
 }
 
 // SubmittedPartial handles a received submitted_partial event
 func (s *FarmerServiceMetrics) SubmittedPartial(resp *types.WebsocketResponse) {
-	// @TODO
+	partial := &types.EventFarmerSubmittedPartial{}
+	err := json.Unmarshal(resp.Data, partial)
+	if err != nil {
+		log.Errorf("Error unmarshalling: %s\n", err.Error())
+		return
+	}
+
+	s.submittedPartials.WithLabelValues(partial.LauncherID).Inc()
+	s.currentDifficulty.WithLabelValues(partial.LauncherID).Set(float64(partial.CurrentDifficulty))
+	s.pointsAckSinceStart.WithLabelValues(partial.LauncherID).Set(float64(partial.PointsAcknowledgedSinceStart))
 }
